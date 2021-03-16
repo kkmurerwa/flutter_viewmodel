daaec3947f261a32405b4be15c8f38bc321e3bb1
kkmurerwa <kkmurerwa@gmail.com>
2021-03-16 10:30:20 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vm_tutorial/classes/counter.dart';
+import 'package:vm_tutorial/pages/home_page.dart';
+
+/// This is a reimplementation of the default Flutter application using provider + [ChangeNotifier].
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Counter()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:vm_tutorial/classes/counter.dart';
+import 'package:provider/provider.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example'),
+      ),
+      body: Center(
+        child: Count(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('increment_floatingActionButton'),
+        onPressed: () => context.read<Counter>().increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Count extends StatelessWidget {
+  const Count({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+        '${context.watch<Counter>().count}',
+        key: const Key('counterState'),
+      style: TextStyle(
+        fontSize: 120,
+        fontWeight: FontWeight.w400
+      ),
+    );
+  }
+}
